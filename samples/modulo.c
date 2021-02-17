7a00ec80c1ff07749d8e6d452c327b67521dc6e6
angrprojdev <angrprojdev@gmail.com>
2021-02-17 14:15:47 +0100

    modulo example

---
+++ samples/modulo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        printf("Two arguments expected.\n");
+        return 1;
+    }
+    int n = *argv[1];
+    int c = 0;
+    while(n != 0)
+    {
+        n = (n + 2) % 100000;
+        c++;
+    }
+    if(c % 10000 == 1)
+    {
+        printf("The lucky number!");
+        return 0;
+    }
+    return 0;
+}
