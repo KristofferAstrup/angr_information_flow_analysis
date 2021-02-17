f07405e9014f81cac78c9586ea8f6edccfdb9754
angrprojdev <angrprojdev@gmail.com>
2021-02-17 13:42:41 +0100

    simple c example

---
+++ samples/simple.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size\n");
+    scanf("%d", &n);
+    int c = 0;
+    while(n != 0)
+    {
+        n = (n + 2) % 100000;
+        c++;
+    }
+    if(c % 10000 == 1)
+    {
+        printf("The lucky number!");
+        return 0;
+    }
+    return 0;
+}
