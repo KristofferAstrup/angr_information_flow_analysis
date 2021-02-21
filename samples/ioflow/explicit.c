87a941e6da65945e1baf6c325598c6c6bf92d088
angrprojdev <angrprojdev@gmail.com>
2021-02-21 13:41:39 +0100

    explicit

--- samples/ioflow/explicit.c
+++ samples/ioflow/explicit.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    if(argc != 2) {
-        return 0;
-    }
     printf("%s\n", "nodanger");
 
-    char* str = argv[1];
-    printf("%s\n", str);
+    if(argc != 2) {
+        char* str = argv[1];
+        printf("%s\n", str);
+    }
 }
