b1883bd2ae0db86e454d0075cfd315458055af47
Kristoffer Astrup <kristoffer.astrup@gmail.com>
2021-02-26 13:20:33 +0100

    upd

--- samples/ioflow/nothing/nothing.py
+++ samples/ioflow/nothing/nothing.py
@@ -1,7 +1,6 @@
 import angr
 import monkeyhex
 import inspect
-import re
 from angr import KnowledgeBase
 from angr.sim_variable import SimRegisterVariable, SimConstantVariable
 from angr.code_location import CodeLocation
@@ -15,10 +14,8 @@ sys.path.append('../../../')
 from customutil import util
 
 def main():
-    proj = angr.Project('implicit_2.out', load_options={'auto_load_libs':False})
-    sym_arg_size = 15
-    arg0 = claripy.BVS('arg0', 8*sym_arg_size)
-    state = proj.factory.entry_state(args=['./implicit_2.out', arg0])
+    proj = angr.Project('nothing.out', load_options={'auto_load_libs':False})
+    state = proj.factory.entry_state()
     simgr = proj.factory.simgr(state)
 
     cfg = proj.analyses.CFGEmulated(
@@ -27,32 +24,37 @@ def main():
         starts=[state.addr], 
         initial_state=state,
         state_add_options=angr.options.refs,
-        context_sensitivity_level = 10
+        context_sensitivity_level = 4
     )
 
     ddg = proj.analyses.DDG(cfg = cfg)
     cdg = proj.analyses.CDG(cfg = cfg)
-    print(dir(cdg))
+
+    util.cfgs(proj, simgr, state)
     return 0
 
-    plot_cdg(cfg, cdg, fname="cdg_2", format="pdf")
-    plot_ddg_data(ddg.data_graph, fname="ddg_2", format="pdf")
-    return 0
+    print('--------')
 
-    highAddresses = [0x401155, 0x401158]
+    lowAddresses = {0x401172}
+    highAddresses = {0x401158, 0x401155}
 
-    branch_addr = 0x401149
-    branch_ins = None
-    for n in cdg.graph.nodes(data=True):
-        if n[0].block_id and n[0].block_id.addr == branch_addr:
-            branch_ins = n[0].instruction_addrs[len(n[0].instruction_addrs)-1]
-            print(hex(branch_ins))
-    
-    isHighContext = False
-    for path in util.find_explicit(proj, ddg, [branch_ins], highAddresses):
+    for path in util.find_explicit(proj, ddg, lowAddresses, highAddresses):
         print(path)
-        isHighContext = True
+        for n in path:
+           print(hex(n.location.ins_addr))
 
+    return 0
 
 if __name__ == "__main__":
     main()
+
+
+
+# for n in util.find_ddg_arg_nodes(proj, ddg):
+#         highAddresses.append(n.location.ins_addr)
+
+
+   # idfer = proj.analyses.Identifier()
+    # for funcInfo in idfer.func_info:
+    #     if(funcInfo.name == "puts"):
+    #         puts_func_info = funcInfo
