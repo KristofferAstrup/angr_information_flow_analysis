e4c148dcfa6eb629a6ba02d7fbd64fdac0b4e3df
Kristoffer Astrup <kristoffer.astrup@gmail.com>
2021-03-10 20:18:53 +0100

    explicit through functions

--- customutil/util.py
+++ customutil/util.py
@@ -1,4 +1,5 @@
 import angr
+import angr.analyses.reaching_definitions.dep_graph as dep_graph
 from angrutils import *
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -111,27 +112,27 @@ def draw_graph(graph, fname="graph.pdf"):
     nx.draw(graph, with_labels=True)
     fig.savefig(fname, dpi=5)
 
-def find_explicit(proj, ddg, lowAddresses=[], highAddresses=[], regBlacklist=None):
-    if regBlacklist == None:
-        regBlacklist = [proj.arch.ip_offset]
-    targetNodes = []
+# def find_explicit(proj, ddg, lowAddresses=[], highAddresses=[], regBlacklist=None):
+#     if regBlacklist == None:
+#         regBlacklist = [proj.arch.ip_offset]
+#     targetNodes = []
     
-    for n in ddg.data_graph.nodes(data=True):
-        if n[0].location.ins_addr in lowAddresses and not isinstance(n[0].variable, SimConstantVariable):
-            if(n[0].variable and isinstance(n[0].variable, SimRegisterVariable) and n[0].variable.reg in regBlacklist):
-                continue
-            targetNodes.append(n[0])
+#     for n in ddg.data_graph.nodes(data=True):
+#         if n[0].location.ins_addr in lowAddresses and not isinstance(n[0].variable, SimConstantVariable):
+#             if(n[0].variable and isinstance(n[0].variable, SimRegisterVariable) and n[0].variable.reg in regBlacklist):
+#                 continue
+#             targetNodes.append(n[0])
 
-    for n in ddg.data_graph.nodes(data=True):
-        if n[0].location.ins_addr in highAddresses and not isinstance(n[0].variable, SimConstantVariable):
-            if n[0].variable and isinstance(n[0].variable, SimRegisterVariable) and n[0].variable.reg in regBlacklist:
-                continue
-            sub = ddg.data_sub_graph(n[0], simplified=False)
-            for targetNode in targetNodes:
-                try:
-                    yield nx.dijkstra_path(sub,n[0],targetNode)
-                except:
-                    pass #No path
+#     for n in ddg.data_graph.nodes(data=True):
+#         if n[0].location.ins_addr in highAddresses and not isinstance(n[0].variable, SimConstantVariable):
+#             if n[0].variable and isinstance(n[0].variable, SimRegisterVariable) and n[0].variable.reg in regBlacklist:
+#                 continue
+#             sub = ddg.data_sub_graph(n[0], simplified=False)
+#             for targetNode in targetNodes:
+#                 try:
+#                     yield nx.dijkstra_path(sub,n[0],targetNode)
+#                 except:
+#                     pass #No path
 
 def find_procedure_nodes(proj, ddg, sim_proc_name, regBlacklist=None):
     if regBlacklist == None:
@@ -209,7 +210,7 @@ def find_cdg_block_nodes(cdg, block_addr):
             yield n
 
 def find_all_descendants_block_address(cfg, cdg_node):
-    for n in nx.descendants(cfg.graph, cdg_node[0]):
+    for n in nx.descendants(cfg.graph, cdg_node):
         if n.block:
             yield n.block.addr
 
@@ -334,10 +335,130 @@ def find_implicit_high_ins_addr(proj, cdg, ddg, cdg_node, highAddresses=[], regB
     return implicit_highs
 
 def find_cdg_node(cdg, block_addr):
-    for n in cdg.graph.nodes(data=True):
-        if n[0].addr == block_addr:
+    for n in cdg.graph.nodes:
+        if n.addr == block_addr:
             return n
     return None
 
-#find branches and test if they create a high context
-#return the instruction adresses of the high context
+def find_cfg_node(cfg, block_addr):
+    for n in cfg.graph.nodes:
+        if n.addr == block_addr:
+            return n
+    return None
+
+
+##########EXPLICIT############
+#Capture all relevant functions (main and all post main in cdg)
+#inclusive
+def get_unique_reachable_function_addresses(cfg, start_node):
+    function_addrs = []
+    for n in nx.descendants(cfg.graph, start_node):
+        if not n.function_address in function_addrs:
+            function_addrs.append(n.function_address)
+    return function_addrs
+
+#Create rda foreach function
+#Create new super graph containing all rda graphs
+def get_super_dep_graph(proj, function_addrs):
+    cfg = proj.analyses.CFGFast() #adds info to kb
+    rda_dep_graph = dep_graph.DepGraph()
+    for func_addr in function_addrs:
+        #print(hex(func_addr))
+        func = proj.kb.functions.function(addr=func_addr)
+        if func == None:
+            print('Warning: ' + str(hex(func_addr)) + ' did not map to any function through kb!')
+            continue
+        rda = proj.analyses.ReachingDefinitions(
+            subject = func,
+            cc = func.calling_convention if func.calling_convention else None,
+            dep_graph = rda_dep_graph,
+            observe_all=True
+        )
+        rda_dep_graph = rda.dep_graph
+    return rda_dep_graph
+
+#find possible paths using the super rda dependence graph
+def find_dep_graph_node(super_dep_graph, ins_addr):
+    for n in super_dep_graph.graph.nodes():
+        if n.codeloc and n.codeloc.ins_addr == ins_addr:
+            return n
+
+def find_dep_graph_nodes(super_dep_graph, ins_addrs):
+    for ins_addr in ins_addrs:
+        yield find_dep_graph_node(super_dep_graph, ins_addr)
+
+def find_explicit(super_dep_graph, lowAddresses, highAddresses):
+    low_nodes = list(find_dep_graph_nodes(super_dep_graph, lowAddresses))
+    high_nodes = list(find_dep_graph_nodes(super_dep_graph, highAddresses))
+    for high_node in high_nodes:
+        for low_node in low_nodes:
+            try:
+                yield nx.dijkstra_path(super_dep_graph.graph, high_node, low_node)
+            except:
+                pass #No path
+
+    # for n in ddg.data_graph.nodes(data=True):
+    #     if n[0].location.ins_addr in lowAddresses and not isinstance(n[0].variable, SimConstantVariable):
+    #         if(n[0].variable and isinstance(n[0].variable, SimRegisterVariable) and n[0].variable.reg in regBlacklist):
+    #             continue
+    #         targetNodes.append(n[0])
+
+    # for n in ddg.data_graph.nodes(data=True):
+    #     if n[0].location.ins_addr in highAddresses and not isinstance(n[0].variable, SimConstantVariable):
+    #         if n[0].variable and isinstance(n[0].variable, SimRegisterVariable) and n[0].variable.reg in regBlacklist:
+    #             continue
+    #         sub = ddg.data_sub_graph(n[0], simplified=False)
+    #         for targetNode in targetNodes:
+    #             try:
+    #                 yield nx.dijkstra_path(sub,n[0],targetNode)
+    #             except:
+    #                 pass #No path
+
+#Augment super graph with edges: 
+#Foreach external(argument-input) try to find a source/caller(argument-output)
+#   Find the block of the external node in the CDG (or call-graph)
+#   Foreach block caller:
+#       If the caller block has a end-node node in it's rsa that corresponds to the external node:
+#           Add edge from this node to the external node
+#       If not, recursively repeat with the caller blocks callers
+def link_externals_to_earliest_definition(super_dep_graph, cdg, cdg_end_nodes):
+    leafs = get_leafs(super_dep_graph.graph)
+    externals = get_externals(super_dep_graph)
+    for external in externals:
+        for nn in list(nx.all_neighbors(super_dep_graph.graph, external)):
+            cdg_node = find_cdg_node(cdg, nn.codeloc.block_addr)
+            matches = find_earliest_matching_definition(external, nn, leafs, cdg_end_nodes, cdg_node)
+            # print('------------')
+            # print(external)
+            # print(nn)
+            # print('----')
+            for match in matches:
+                # print(match)
+                super_dep_graph.graph.add_edge(match, nn)
+
+#external is the target external node from which the target is child
+#the target is the node to which we want to link, we need to know it's block_addr
+#leafs are the end-nodes of the super_dep_graph
+#the cdg_node is currently inspected node
+def find_earliest_matching_definition(external, target, leafs, cdg_end_nodes, cdg_node):
+    if cdg_node.block and target.codeloc.block_addr != cdg_node.block.addr:
+        for leaf in leafs:
+            if leaf.codeloc and leaf.codeloc.block_addr == cdg_node.block.addr:
+                if leaf.atom == external.atom:
+                    return [leaf]
+    if cdg_node in cdg_end_nodes:
+        return []
+    caller_blocks = cdg_node.predecessors
+    matches = []
+    for caller_block in caller_blocks:
+        matches += find_earliest_matching_definition(external, target, leafs, cdg_end_nodes, caller_block)
+    return matches
+
+def get_externals(super_dep_graph):
+    for n in super_dep_graph.graph.nodes:
+        if isinstance(n.codeloc, angr.analyses.reaching_definitions.external_codeloc.ExternalCodeLocation):
+            yield n
+
+def get_leafs(graph):
+    leaf_nodes = [node for node in graph.nodes() if graph.in_degree(node)!=0 and graph.out_degree(node)==0]
+    return leaf_nodes
--- samples/ioflow/mixed/mixed.py
+++ samples/ioflow/mixed/mixed.py
@@ -30,81 +30,34 @@ def main():
         state_add_options=angr.options.refs,
         context_sensitivity_level = 0
     )
-
     ddg = proj.analyses.DDG(cfg = cfg)
     cdg = proj.analyses.CDG(cfg = cfg)
-    util.link_similar_ins_regs(ddg)
-    #util.link_similar_mem(ddg)
-    #return 
-    # vfg = proj.analyses.VFG(cfg = cfg, function_start=proj.entry,
-    #     context_sensitivity_level=4, interfunction_level=4,
-    #     remove_options={ angr.options.OPTIMIZE_IR })
-    # util.draw_graph(vfg.graph, "vfg.pdf")
-    # vsa = angr.analyses.VSA_DDG(vfg=vfg,context_sensitivity_level=4,interfunction_level=4,keep_data=True)
-    # util.draw_graph(vsa.graph, fname="vsa.pdf")
-    
-    print('---')
-    for path in util.find_explicit(proj, ddg, [0x401174], [0x4011c5]):
-        print(path)
-    print('---')
-    for path in util.find_explicit(proj, ddg, [0x40115d], [0x401174]):
-        print(path)
-    
 
-    return
-
-    util.clear_constant_ddg_nodes(ddg)
-    #libc_start_main_node = list(util.find_cdg_block_nodes(cdg, 0x4011a0))[0]
-    #print(libc_start_main_node)
-    #block_addr_whitelist = list(util.find_all_descendants_block_address(cfg, libc_start_main_node))
-    #block_addr_whitelist.append(libc_start_main_node[0].block.addr)
-    #print(util.hexlist(block_addr_whitelist))
-    #util.filter_ddg_block_whitelist(ddg, block_addr_whitelist)
-    imp_nodes = list(util.find_ddg_nodes(ddg, 0x40115d))
-    print(imp_nodes)
-    imp_nodes += util.get_all_ancestors_of_ddg_ins(ddg, imp_nodes)
-    util.filter_ddg_node_whitelist(ddg, imp_nodes)
-    plot_ddg_data(ddg.data_graph, "ddg_40115d", format="pdf", asminst=True, vexinst=False)
-    
-    # plot_cdg(cfg, cdg, "cdg", format="pdf")
-    # return 
-
-    highAddresses = [0x4011ac, 0x4011af]
-
-    for path in util.find_explicit(proj, ddg, [0x401195], highAddresses):
-        #if len(path) > 1:
-        print(path)
-
-    return 
-
-    mainAddress = 0x4011a0
+    high_addrs = [0x4011ac, 0x4011af]
+    start_addr = 0x4011a0
 
     puts_proc = "puts"
-
     arg_regs = util.get_sim_proc_reg_args(proj, puts_proc)
-
     subject_addrs = []
     for wrap_addr in util.get_sim_proc_function_wrapper_addrs(proj, puts_proc):
         for caller in util.get_function_node(cdg, wrap_addr).predecessors:
             for reg in arg_regs:
                 offset, size = proj.arch.registers[reg.reg_name]
-                for occ_node in util.find_first_reg_occurences_from_cdg_node(cdg, ddg, caller, offset, mainAddress):
+                for occ_node in util.find_first_reg_occurences_from_cdg_node(cdg, ddg, caller, offset, start_addr):
                     subject_addrs.append(occ_node[0].location.ins_addr)
-    print('Subjects:')
-    print(util.hexlist(subject_addrs))
-    
-    print('Explicits:')
-    for path in util.find_explicit(proj, ddg, subject_addrs, highAddresses):
-        print("Explicit flow:")
-        print(path)
 
-    print('Implicits:')
-    main_node = util.find_cdg_node(cdg, mainAddress)
-    for implicit_high in util.find_implicit_high_ins_addr(proj, cdg, ddg, main_node, highAddresses):
-        #print("subject: " + str(subject_addrs) + " | highs: " + str([hex(implicit_high)]))
-        for path in util.find_explicit(proj, ddg, subject_addrs, [implicit_high]):
-            print("Implicit flow:")
-            print(path)
+    start_node = util.find_cfg_node(cfg, start_addr)
+    func_addrs = util.get_unique_reachable_function_addresses(cfg, start_node)
+    super_dep_graph = util.get_super_dep_graph(proj, func_addrs)
+
+    util.link_externals_to_earliest_definition(super_dep_graph, cdg, [start_node])
+    
+    util.draw_graph(super_dep_graph.graph, fname="super_rda.pdf")
+
+    for path in util.find_explicit(super_dep_graph, subject_addrs, high_addrs):
+        print('----Path-----')
+        for step in path:
+            print(hex(step.codeloc.ins_addr))
 
     return 0
 
