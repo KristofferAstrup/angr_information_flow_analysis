d6f75972558595415bb061c2438963abbbe0a209
angrprojdev <angrprojdev@gmail.com>
2021-04-03 11:37:22 +0200

    implicit4

---
+++ samples/ioflow/implicit4/implicit4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int t = 0;
+    if(argv[1][0] > 60) {
+        t = 1;
+    }
+    if(argv[1][0] > 65) {
+        t = 2;
+    }
+    if(t > 1) {
+        printf("%s\n", "!");
+    }
+}
