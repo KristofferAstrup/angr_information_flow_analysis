a02aea49cda7220ef93732e4ae9e8c4bd99aeb22
Kristoffer Astrup <kristoffer.astrup@gmail.com>
2021-04-23 17:18:40 +0200

    Cleanup

--- samples/ioflow/implicit/implicit.py
+++ samples/ioflow/implicit/implicit.py
@@ -1,11 +1,13 @@
 import angr
 import claripy
 import sys
+import networkx
 sys.path.append('../../../')
-from customutil import util_analysis
+from customutil import util_analysis, util_out
 
 def main():
     proj = angr.Project('implicit.out', load_options={'auto_load_libs':False})
+    
     sym_arg_size = 15
     arg0 = claripy.BVS('arg0', 8*sym_arg_size)
     state = proj.factory.entry_state(args=['./implicit.out', arg0])
@@ -15,7 +17,8 @@ def main():
 
     ifa = util_analysis.InformationFlowAnalysis(proj=proj,state=state,start_addr=start_addr,high_addrs=high_addrs)
     ifa.find_and_add_subject_addrs("puts")
-    leaks = ifa.find_all_leaks()
+    ifa.find_all_leaks()
+    
     return
   
 if __name__ == "__main__":
