fad8dfd9815af8b3e3f43167a49403e333ad62a7
angrprojdev <angrprojdev@gmail.com>
2021-05-24 16:17:12 +0200

    equal_sleep update

--- samples/timing/equal_sleep/equal_sleep.c
+++ samples/timing/equal_sleep/equal_sleep.c
@@ -3,11 +3,11 @@
 int main(int argc, char** argv) {
     printf("Hello\n");
     if(argv[1][0] == 'a') {
-        for(int i=1; i<=3; i++) {
-            sleep(i);
+        for(int i=0; i<3; i++) {
+            sleep(1);
         }
     } else {
-        sleep(6);   
+        sleep(3);   
     }
     printf("Goodbye\n");
     return 0;
