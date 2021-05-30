f5a882f24583bb482c2723d3216929fe58c6446d
angrprojdev <angrprojdev@gmail.com>
2021-05-30 16:03:41 +0200

    low inf loop no leak example

---
+++ samples/termination/low_inf_loop_no_leak/low_inf_loop_no_leak.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    if(argc == 2) {
+        printf("EqualProgress");
+    } else {
+        printf("EqualProgress");
+    }
+    while(argv[1][0] == 'a'){}
+    return 0;
+}
